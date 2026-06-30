c7cce57407f343981d8defc9ddd38bcfd12801b8
shoaib Akram <shoaibakram2003@gmail.com>
2026-06-30 21:40:50 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Tree Component Assignment</h1>
+      <p>Demo app will be wired up here in Phase 5.</p>
+    </div>
+  )
+}
+
+export default App
